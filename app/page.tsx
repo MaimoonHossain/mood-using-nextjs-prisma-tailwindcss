d121ed480e6cb4460ff224928a9a8a07c0ed3c45
Maimoon Hossain <ashikasitosh740@gmail.comm>
2023-10-13 23:02:49 +0600

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
   return (
     <div className='w-screen h-screen bg-black text-white flex justify-center items-center'>
       <div className='w-full max-w-[600px] mx-auto'>
@@ -11,7 +16,7 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-          <Link href='/journal'>
+          <Link href={href}>
             <button className='bg-sky-400 px-4 py-2 rounded text-xl'>
               get started
             </button>
